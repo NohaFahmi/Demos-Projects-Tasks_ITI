47ca5af299d1de80cd86a8f56feb7a1f2336ba23
Noha M <33437197+NohaaAa@users.noreply.github.com>
2020-09-29 12:16:01 +0200

    OOP using C#: day 2

---
+++ OOP using C#/D02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+
+namespace D01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.Read();
+
+            //Console.WriteLine("Hello Front ");
+            //Console.ReadLine();
+
+            //int x = 15;
+            //float y = 12.5f; //the f char. to tell the program that I want it to be float not double
+            //decimal z = 3.4m; //the m to tell that I want to be decimal not double
+            //Those chars. is useful only in values not variables.
+            //float f = 3.2f;
+            //Console.WriteLine(x);
+            //Console.WriteLine(f);
+
+            //string String = "Num X =";
+
+
+            //Console.WriteLine($" num= {x:c}");
+            //Console.WriteLine($" num= {x:x}");
+            //Console.WriteLine($" Num= {x:d}");
+            //Console.WriteLine($" num= {x:e}");
+            //Console.WriteLine($" num= {x:f}");
+            //Console.WriteLine($" num= {x:g}");
+            //Console.WriteLine($" num= {x:n}");
+
+           
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ OOP using C#/Task3_Solution1/Task3_Solution1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task3_Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Task3: How can you count the occurrence of 1 from 1 to 99,999,999 (1 short of 100 million) 
+            //and total up how many 1s were there.
+
+            var watch = new System.Diagnostics.Stopwatch();
+
+            int[] arr = new int[100000000];
+
+            int count = 0;
+
+            #region Solution #1
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] += i + 1;
+            }
+            
+            watch.Start();
+
+            foreach (int num in arr)
+            {
+                string numToStr = num.ToString(); 
+
+                for (int j = 0; j < numToStr.Length; j++)
+                {
+                    if (numToStr[j] == '1')
+                    {
+                        count += 1;
+                    }
+                }
+
+            }
+
+            Console.WriteLine(count);
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms"); //10110ms
+
+            #endregion
+
+
+        }
+    }
+}
