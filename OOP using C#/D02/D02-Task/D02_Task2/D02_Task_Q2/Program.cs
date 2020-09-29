68b9d1c23a98b607dff97209f7f888542f792dcb
Noha M <nm1488@fayoum.edu.eg>
2020-09-29 12:19:52 +0200

    OOP Using C#:day 2

---
+++ OOP using C#/D02/D02-Task/D02_Task2/D02_Task_Q2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace D02_Task_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q.2>>
+
+            //•	Given a list of space separated words, reverse the order of the words.
+
+            string[] inputArr =
+            {
+                "This is Task 2",
+                "Wish you Luck",
+                "All your Base",
+                "Word",
+                "This is a Test"
+            };
+
+            foreach(string ele in inputArr)
+            {
+                string result = string.Join(" ", ele.Split(" ").Reverse());
+
+                Console.WriteLine(result);
+            }
+
+            
+
+            #endregion
+        }
+    }
+}
